0523ac9118357bc9742df1ce5e006c0e2004798e
Palak Verma <pverma.lenovo@gmail.com>
2026-07-05 15:19:28 +0530

    My Js task 1

---
+++ palak.js
@@ -0,0 +1,40 @@
+alert("My Js task 1");
+
+let name = "Palak Verma";
+let age = 18;
+const college = "Regional college of management";
+const course = "Bachelor of Computer Applications (BCA)";
+let city = "Bhubaneswar";
+let isStudent = true;
+
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("College:", college);
+console.log("Course:", course);
+console.log("City:", city);
+console.log("Is Student:", isStudent);
+
+document.write("<h2>Student Details</h2>");
+
+document.write("Name: " + name + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("College: " + college + "<br>");
+document.write("Course: " + course + "<br>");
+document.write("City: " + city + "<br>");
+document.write("Is Student: " + isStudent + "<br><br>");
+
+
+let myString = "Hello JavaScript";
+let myNumber = 100;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+
+
+console.log("String:", typeof myString);
+console.log("Number:", typeof myNumber);
+console.log("Boolean:", typeof myBoolean);
+console.log("Null:", typeof myNull);      
+console.log("Undefined:", typeof myUndefined);
