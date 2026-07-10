acdbf8ba51cd4e78066eb718272d03872a9f02ef
Palak Verma <pverma.lenovo@gmail.com>
2026-07-11 00:11:09 +0530

    update Js task 1 and 2

--- palak.js
+++ palak.js
@@ -44,3 +44,101 @@ console.log("Number:", typeof myNumber);
 console.log("Boolean:", typeof myBoolean);
 console.log("Null:", typeof myNull);      
 console.log("Undefined:", typeof myUndefined);
+// ======================================
+// Task - 2
+// ======================================
+
+const studentName = "Palak Verma";
+const rollNumber = 101;
+let age = 18;
+const collegeName = "Regional college of Management";
+const course = "Bachelor of Computer Applications (BCA)";
+const city = "Bhubaneswar";
+const isStudent = true;
+
+let htmlMarks = 90;
+let cssMarks = 88;
+let javascriptMarks = 85;
+let databaseMarks = 80;
+let gitMarks = 87;
+
+const totalMarks = htmlMarks + cssMarks + javascriptMarks + databaseMarks + gitMarks;
+const maximumMarks = 500;
+const averageMarks = totalMarks / 5;
+
+console.log("STUDENT RESULT SUMMARY");
+console.log("Student Name: " + studentName);
+console.log("Roll Number: " + rollNumber);
+console.log("Age: " + age);
+console.log("College: " + collegeName);
+console.log("Course: " + course);
+console.log("City: " + city);
+console.log("Is Student: " + isStudent);
+
+console.log("HTML Marks: " + htmlMarks);
+console.log("CSS Marks: " + cssMarks);
+console.log("JavaScript Marks: " + javascriptMarks);
+console.log("Database Marks: " + databaseMarks);
+console.log("Git Marks: " + gitMarks);
+
+console.log("Total Marks: " + totalMarks);
+console.log("Maximum Marks: " + maximumMarks);
+console.log("Average Marks: " + averageMarks);
+
+let myString = "Hello JavaScript";
+let myNumber = 500;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+console.log("String: " + myString);
+console.log("Number: " + myNumber);
+console.log("Boolean: " + myBoolean);
+console.log("Null: " + myNull);
+console.log("Undefined: " + myUndefined);
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+
+let num = 20;
+
+console.log("Addition: " + (10 + 5));
+console.log("Subtraction: " + (20 - 8));
+console.log("Multiplication: " + (7 * 6));
+console.log("Division: " + (30 / 5));
+console.log("Modulus: " + (25 % 4));
+
+num++;
+console.log("Increment: " + num);
+
+num--;
+console.log("Decrement: " + num);
+
+num += 10;
+console.log("After += : " + num);
+
+num -= 5;
+console.log("After -= : " + num);
+
+num *= 2;
+console.log("After *= : " + num);
+
+num /= 5;
+console.log("After /= : " + num);
+
+console.log("HTML > CSS: " + (htmlMarks > cssMarks));
+console.log("HTML < CSS: " + (htmlMarks < cssMarks));
+console.log("HTML >= CSS: " + (htmlMarks >= cssMarks));
+console.log("HTML <= CSS: " + (htmlMarks <= cssMarks));
+console.log("HTML == CSS: " + (htmlMarks == cssMarks));
+console.log("HTML === CSS: " + (htmlMarks === cssMarks));
+console.log("HTML != CSS: " + (htmlMarks != cssMarks));
+
+let numberValue = 100;
+let stringValue = "100";
+
+console.log("Using == : " + (numberValue == stringValue));
+console.log("Using === : " + (numberValue === stringValue));
